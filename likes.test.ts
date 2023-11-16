31b1f1769d7c727c9a1f58af2f2aac4b97aa2aef
Bernhard Schenk <bernhardschenk@gmx.de>
2023-11-16 17:54:25 +0100

    feat: any single person can like this
    Co-authored-by: Stefan Boos <kontakt@boos.systems>
    Co-authored-by: Niklas Rueber <nrueber@igus.net>

--- likes.test.ts
+++ likes.test.ts
@@ -1,9 +1,18 @@
 function likes(param: any[]) {
+    if (param.length > 0) {
+        return `${param[0]} likes this`;
+    }
     return "no one likes this";
 }
 
 describe('first_test', () => {
     it("no one likes this", () => {
         expect(likes([])).toBe("no one likes this");
+    })
+    it("Bernhard likes this", () => {
+        expect(likes(["Bernhard"])).toBe("Bernhard likes this");
+    })
+    it("Stefan likes this", () => {
+        expect(likes(["Stefan"])).toBe("Stefan likes this");
     })
 });

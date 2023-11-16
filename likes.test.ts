785503b60c1de2c9ca232adf256a16b6658f827e
Niklas Rueber <nrueber@igus.net>
2023-11-16 17:46:19 +0100

    feat: no one likes this
    Co-authored-by: Stefan Boos <kontakt@boos.systems>
    Co-authored-by: Bernhard Schenk <bernhardschenk@gmx.de>
    Co-authored-by: Michael Mahlberg <mm@consulting-guild.de>

--- likes.test.ts
+++ likes.test.ts
@@ -1,5 +1,9 @@
+function likes(param: any[]) {
+    return "no one likes this";
+}
+
 describe('first_test', () => {
-    it("should work - but useless", () => {
-        expect(false).toBe(false);
+    it("no one likes this", () => {
+        expect(likes([])).toBe("no one likes this");
     })
 });
